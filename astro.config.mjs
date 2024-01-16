90f76c527dcf01a511eaa8bb018eb35815407626
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-16 16:41:51 -0600

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,9 @@ export default defineConfig({
     locales: ['en', 'es'],
     defaultLocale: 'en',
     routing: {
-      prefixDefaultLocale: false,
+      prefixDefaultLocale: false
     }
   },
+  output: "server",
+  adapter: vercel()
 });
