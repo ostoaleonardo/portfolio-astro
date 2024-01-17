835a2a4a9e2e1e6b427ae300fea8e0ca5417f4b9
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-17 14:33:40 -0600

    Reset

--- src/pages/api/discord.ts
+++ src/pages/api/discord.ts
@@ -1,18 +1,32 @@
+import type { APIRoute } from "astro"
+
 const ENDPOINT = "https://api.lanyard.rest/v1/users/771148579356016650"
 const ASSETS_URL = "https://cdn.discordapp.com/app-assets/"
 
-export const getDiscordStatus = async () => {
+export const GET: APIRoute = async () => {
     const discord = await fetch(ENDPOINT)
         .then((res) => res.json())
         .then((data) => data.data)
         .then((data) => formatDiscordStatus(data))
         .catch(() => { return { status: "offline", activity: "", details: "", image: "" } })
 
-    return discord
+    return new Response(
+        JSON.stringify(discord)
+    )
 }
 
 const formatDiscordStatus = (discord: any) => {
     const { activities, discord_status } = discord
+
+    if (activities.length === 0) {
+        return {
+            status: discord_status,
+            activity: "",
+            details: "",
+            image: ""
+        }
+    }
+
     const { name, details, assets, application_id } = activities[0]
     const { large_image } = assets
 
