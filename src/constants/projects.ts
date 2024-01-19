e85e7e830b736481b11a001e018ef0dee7314c2e
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-18 20:13:26 -0600

    Add projects

---
+++ src/constants/projects.ts
@@ -0,0 +1,80 @@
+import tailtools1 from '../assets/projects/tailtools1.webp';
+import tailtools2 from '../assets/projects/tailtools2.webp';
+import tailtools3 from '../assets/projects/tailtools3.webp';
+import tailtools4 from '../assets/projects/tailtools4.webp';
+import wavestify1 from '../assets/projects/wavestify1.webp';
+import wavestify2 from '../assets/projects/wavestify2.webp';
+import store1 from '../assets/projects/store1.webp';
+import store2 from '../assets/projects/store2.webp';
+import store3 from '../assets/projects/store3.webp';
+import movies1 from '../assets/projects/movies1.webp';
+import movies2 from '../assets/projects/movies2.webp';
+import minecraft1 from '../assets/projects/minecraft1.webp';
+import minecraft2 from '../assets/projects/minecraft2.webp';
+import minecraft3 from '../assets/projects/minecraft3.webp';
+
+export const PROJECTS = [
+    {
+        title: 'Tailtools',
+        type: 'Web app',
+        description: 'A web app that you can use to generate color palettes, gradients, and more for Tailwind CSS.',
+        images: [
+            tailtools1,
+            tailtools2,
+            tailtools3,
+            tailtools4,
+        ],
+        technologies: ['React', 'TypeScript', 'Tailwind', 'NextUI'],
+        github: 'https://github.com/ostoaleonardo/tailtools',
+    },
+    {
+        title: 'Wavestify',
+        type: 'Web app',
+        description: 'A web app that shows your Spotify stats.',
+        images: [
+            wavestify1,
+            wavestify2,
+        ],
+        technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind', 'Framer Motion'],
+        demo: 'https://wavestify.vercel.app/',
+        github: 'https://github.com/ostoaleonardo/spotify-stats',
+    },
+    {
+        title: 'E-commerce',
+        type: 'Web app',
+        description: 'A web app that simulates an e-commerce store, where you can add products to the cart and make a purchase.',
+        technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind', 'Framer Motion'],
+        images: [
+            store1,
+            store2,
+            store3,
+        ],
+        demo: 'https://storecommerce.vercel.app/',
+        github: 'https://github.com/ostoaleonardo/store-e-commerce',
+    },
+    {
+        title: 'Movies Stats',
+        type: 'Web app',
+        description: 'A web app that shows movies stats, like the most popular movies, the best rated, etc.',
+        technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind', 'Framer Motion'],
+        images: [
+            movies1,
+            movies2,
+        ],
+        demo: 'https://movies-stats.vercel.app/',
+        github: 'https://github.com/ostoaleonardo/movies-stats',
+    },
+    {
+        title: 'Minecraft Three.js',
+        type: 'Web app',
+        description: 'Based on the game Minecraft, this web app allows you to create a world with blocks and move around it.',
+        technologies: ['React', 'JavaScript', 'Three.js', 'Tailwind'],
+        images: [
+            minecraft1,
+            minecraft2,
+            minecraft3,
+        ],
+        demo: 'https://minecraft-three-js.vercel.app/',
+        github: 'https://github.com/ostoaleonardo/minecraft-three-js',
+    },
+]
