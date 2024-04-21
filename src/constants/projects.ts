9d82ae20f10495a2d130c4564d61d4bb1e4348ff
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-21 15:37:53 -0600

    Update projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,11 +4,6 @@ import tailtools3 from '@/assets/projects/tailtools3.webp';
 import tailtools4 from '@/assets/projects/tailtools4.webp';
 import wavestify1 from '@/assets/projects/wavestify1.webp';
 import wavestify2 from '@/assets/projects/wavestify2.webp';
-import store1 from '@/assets/projects/store1.webp';
-import store2 from '@/assets/projects/store2.webp';
-import store3 from '@/assets/projects/store3.webp';
-import movies1 from '@/assets/projects/movies1.webp';
-import movies2 from '@/assets/projects/movies2.webp';
 import minecraft1 from '@/assets/projects/minecraft1.webp';
 import minecraft2 from '@/assets/projects/minecraft2.webp';
 import minecraft3 from '@/assets/projects/minecraft3.webp';
@@ -22,7 +17,7 @@ export const PROJECTS = [
             tailtools1,
             tailtools2,
             tailtools3,
-            tailtools4,
+            tailtools4
         ],
         technologies: ['React', 'TypeScript', 'Tailwind', 'NextUI'],
         demo: 'https://tailtools.app/',
@@ -34,36 +29,11 @@ export const PROJECTS = [
         description: 'A web app that shows your Spotify stats.',
         images: [
             wavestify1,
-            wavestify2,
+            wavestify2
         ],
         technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind'],
         demo: 'https://wavestify.vercel.app/',
         github: 'https://github.com/ostoaleonardo/spotify-stats',
-    },
-    {
-        title: 'E-commerce',
-        type: 'Web app',
-        description: 'A web app that simulates an e-commerce store, where you can add products to the cart and make a purchase.',
-        technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind'],
-        images: [
-            store1,
-            store2,
-            store3,
-        ],
-        demo: 'https://storecommerce.vercel.app/',
-        github: 'https://github.com/ostoaleonardo/store-e-commerce',
-    },
-    {
-        title: 'Movies Stats',
-        type: 'Web app',
-        description: 'A web app that shows movies stats, like the most popular movies, the best rated, etc.',
-        technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind'],
-        images: [
-            movies1,
-            movies2,
-        ],
-        demo: 'https://movies-stats.vercel.app/',
-        github: 'https://github.com/ostoaleonardo/movies-stats',
     },
     {
         title: 'Minecraft Three.js',
@@ -73,9 +43,9 @@ export const PROJECTS = [
         images: [
             minecraft1,
             minecraft2,
-            minecraft3,
+            minecraft3
         ],
         demo: 'https://minecraft-three-js.vercel.app/',
         github: 'https://github.com/ostoaleonardo/minecraft-three-js',
-    },
+    }
 ]
