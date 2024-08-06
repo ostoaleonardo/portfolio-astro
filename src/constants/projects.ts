a15ad1ee51fdd7eaa650ba5ebe43e7641deb9b68
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-08-05 21:16:02 -0600

    Update projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,9 +4,6 @@ import tailtools3 from '@/assets/projects/tailtools3.webp';
 import tailtools4 from '@/assets/projects/tailtools4.webp';
 import wavestify1 from '@/assets/projects/wavestify1.webp';
 import wavestify2 from '@/assets/projects/wavestify2.webp';
-import minecraft1 from '@/assets/projects/minecraft1.webp';
-import minecraft2 from '@/assets/projects/minecraft2.webp';
-import minecraft3 from '@/assets/projects/minecraft3.webp';
 
 export const PROJECTS = [
     {
@@ -34,18 +31,5 @@ export const PROJECTS = [
         technologies: ['React', 'JavaScript', 'NextUI', 'Tailwind'],
         demo: 'https://wavestify.vercel.app/',
         github: 'https://github.com/ostoaleonardo/spotify-stats',
-    },
-    {
-        title: 'Minecraft Three.js',
-        type: 'Web app',
-        description: 'Based on the game Minecraft, this web app allows you to create a world with blocks and move around it.',
-        technologies: ['React', 'JavaScript', 'Three.js', 'Tailwind'],
-        images: [
-            minecraft1,
-            minecraft2,
-            minecraft3
-        ],
-        demo: 'https://minecraft-three-js.vercel.app/',
-        github: 'https://github.com/ostoaleonardo/minecraft-three-js',
     }
 ]
